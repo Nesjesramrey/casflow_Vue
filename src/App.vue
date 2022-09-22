f7d315ec0a201f05f78fcaaae650ff47a78c0f45
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-09-22 05:44:20 -0500

    add splashscreen

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <HomePrincipal />
+  <Suspense>
+    <template #default>
+      <HomePrincipal />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import HomePrincipal from "./components/HomePrincipal.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
-    HomePrincipal,
+    SplashScreen,
+    HomePrincipal: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/HomePrincipal.vue"));
+          }, 2050);
+        })
+    ),
   },
 };
 </script>
 
-<style></style>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
