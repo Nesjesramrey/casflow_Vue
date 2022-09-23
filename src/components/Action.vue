6ca7a0bdca15255a63be3eab5578043d28d50a4a
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-09-23 01:35:30 -0500

    add form

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -3,7 +3,33 @@
   <button @click="showModal = true">Agregar movimiento</button>
   <teleport to="#app">
     <Modal v-show="showModal" @close="showModal = false">
-      Aqui formulario
+      <form @submit.prevent="submit">
+        <div class="field">
+          <label>Título</label>
+          <input type="text" v-model="title" />
+        </div>
+        <div class="field">
+          <label>Monto</label>
+          <input type="number" v-model="amount" />
+        </div>
+        <div class="field">
+          <label>Descripción</label>
+          <textarea rows="4" v-model="description"></textarea>
+        </div>
+        <div class="field">
+          <label class="radio-label">
+            <input type="radio" v-model="movementType" value="Ingreso" />
+            <span>Ingreso</span>
+          </label>
+          <label class="radio-label">
+            <input type="radio" v-model="movementType" value="Gasto" />
+            <span>Gasto</span>
+          </label>
+        </div>
+        <div class="action">
+          <button>Agregar movimiento</button>
+        </div>
+      </form>
     </Modal>
   </teleport>
 </template>
@@ -11,7 +37,16 @@
 <script setup>
 import { ref } from "vue";
 import Modal from "./Modal.vue";
+
 const showModal = ref(false);
+const title = ref("");
+const amount = ref(0);
+const description = ref("");
+const movementType = ref("Ingreso");
+
+const submit = () => {
+  showModal.value = !showModal.value;
+};
 </script>
 
 <style scoped>
@@ -25,22 +60,27 @@ button {
   border-radius: 60px;
   box-sizing: border-box;
 }
+
 form {
   font-size: 1.24rem;
   width: 100%;
 }
+
 form .action {
   padding: 0 24px;
 }
+
 .field {
   display: flex;
   justify-content: space-between;
   flex-direction: column;
   padding: 16px 24px;
 }
+
 label {
   margin-bottom: 8px;
 }
+
 input,
 textarea {
   font-size: 1.24rem;
@@ -48,18 +88,22 @@ textarea {
   border-radius: 8px;
   padding: 8px;
 }
+
 input[type="number"] {
   text-align: right;
 }
+
 .radio-label {
   display: flex;
   align-items: center;
   margin-top: 8px;
 }
+
 .radio-label span {
   margin-top: 4px;
   margin-left: 8px;
 }
+
 input[type="radio"] {
   appearance: none;
   width: 1.24rem;
@@ -68,6 +112,7 @@ input[type="radio"] {
   border: 2px solid var(--brand-blue);
   border-radius: 50%;
 }
+
 input[type="radio"]:checked {
   background-color: var(--brand-blue);
 }
