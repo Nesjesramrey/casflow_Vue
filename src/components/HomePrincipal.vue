a018b4effce9ffa4d1d9a4fefbb04071d91528e4
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-09-23 18:04:51 -0500

    function add and remove

--- src/components/HomePrincipal.vue
+++ src/components/HomePrincipal.vue
@@ -14,12 +14,12 @@
           <Graphic :amounts="amounts" />
         </template>
         <template #action>
-          <Action />
+          <Action @create="create" />
         </template>
       </Resume>
     </template>
     <template #movements>
-      <Movements :movements="movements" />
+      <Movements :movements="movements" @remove="remove" />
     </template>
   </Layout>
 </template>
@@ -31,6 +31,7 @@ import Resume from "./Resume/Index.vue";
 import Action from "./Action.vue";
 import Movements from "./Movements/Index.vue";
 import Graphic from "./Resume/Graphic.vue";
+
 export default {
   components: {
     Layout,
@@ -50,70 +51,70 @@ export default {
           title: "Movimiento 1",
           description: "Lorem ipsum dolor sit amet",
           amount: 100,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 1,
           title: "Movimiento 2",
           description: "Lorem ipsum dolor sit amet",
           amount: 200,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 2,
           title: "Movimiento 3",
           description: "Lorem ipsum dolor sit amet",
           amount: 500,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 3,
           title: "Movimiento 4",
           description: "Lorem ipsum dolor sit amet",
           amount: 200,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 4,
           title: "Movimiento 5",
           description: "Lorem ipsum dolor sit amet",
           amount: -400,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 5,
           title: "Movimiento 6",
           description: "Lorem ipsum dolor sit amet",
           amount: -600,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 6,
           title: "Movimiento 7",
           description: "Lorem ipsum dolor sit amet",
           amount: -300,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 7,
           title: "Movimiento 8",
           description: "Lorem ipsum dolor sit amet",
           amount: 100,
-          time: new Date("02-09-2022"),
+          time: new Date("02-01-2022"),
         },
         {
           id: 8,
           title: "Movimiento 9",
           description: "Lorem ipsum dolor sit amet",
           amount: 300,
-          time: new Date("01-09-2022"),
+          time: new Date("01-01-2022"),
         },
         {
           id: 9,
           title: "Movimiento 10",
           description: "Lorem ipsum dolor sit amet",
           amount: 500,
-          time: new Date("01-09-2022"),
+          time: new Date("01-01-2022"),
         },
       ],
     };
@@ -124,16 +125,27 @@ export default {
         .filter((m) => {
           const today = new Date();
           const oldDate = today.setDate(today.getDate() - 30);
+
           return m.time > oldDate;
         })
         .map((m) => m.amount);
 
       return lastDays.map((m, i) => {
         const lastMovements = lastDays.slice(0, i);
+
         return lastMovements.reduce((suma, movement) => {
           return suma + movement;
         }, 0);
       });
+    },
+  },
+  methods: {
+    create(movement) {
+      this.movements.push(movement);
+    },
+    remove(id) {
+      const index = this.movements.findIndex((m) => m.id === id);
+      this.movements.splice(index, 1);
     },
   },
 };
